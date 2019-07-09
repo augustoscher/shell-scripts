2b2e6713198f0485da788bab935f962130935923
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-07-09 13:05:55 -0300

    test file

---
+++ Test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#########################################################################
+#                                                                       #
+# Teste.sh - Testando                                                   #
+#                                                                       #
+# Autor: Augusto Scher - augustoscher@gmail.com                         #
+# Data Criação: DD/MM/YYYY                                              #
+#                                                                       #
+# Descrição: Meu teste                                                  #
+#                                                                       #
+# Exemplo de uso: ./Teste.sh                                            #
+#                                                                       #
+#########################################################################
+
+MINHAVARIAVEL="Oi sou uma variavel"
+
+clear
+echo "========= Primeiro Script ========"
+echo ""
+echo "Conteudo da minha variavel: $MINHAVARIAVEL"
+echo ""
+echo "Fim do Script"
+
