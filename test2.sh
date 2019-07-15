c826fc1a98bdbde98d891599b6774ce173d643e4
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-07-14 23:58:58 -0300

    test2

--- test2.sh
+++ test2.sh
@@ -1,1 +1,17 @@
-echo "test"
+#!/bin/bash
+
+#########################################################################
+#                                                                       #
+# test2.sh - Testando2                                                   #
+#                                                                       #
+# Autor: Augusto Scher - augustoscher@gmail.com                         #
+# Data Criação: 14/07/2019                                              #
+#                                                                       #
+# Descrição: Meu teste2                                                #
+#                                                                       #
+# Exemplo de uso: ./teste2.sh                                            #
+#                                                                       #
+#########################################################################
+
+
+echo "test2"
